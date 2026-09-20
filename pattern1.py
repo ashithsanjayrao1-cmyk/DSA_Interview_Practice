4c23482add1b4fa3a6102ef4ab0afa68dcea60de
Ashith Sanjay Rao <ashithsanjayrao1@gmail.com>
2026-09-20 13:56:34 +0530

    DSA practice Day1

---
+++ pattern1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def patter1(self,n):
+        for i in range(n):
+            for j in range(n):
+                print("*",end = '')
+
+            print()
+
+    my_solution = Solution()
+
+    my_solution.patter1(5)
