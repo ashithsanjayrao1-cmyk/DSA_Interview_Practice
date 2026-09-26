ae48398a106fda20e97791a3d65adae149371d68
Ashith Sanjay Rao <ashithsanjayrao1@gmail.com>
2026-09-26 20:30:02 +0530

    DSA practice

--- Array/second_largest.py
+++ Array/second_largest.py
@@ -1,28 +1,85 @@
+# class Solution():
+#     def secondLar(self,arr):
+#         n = len(arr)
+
+#         if n < 2:
+#             return -1
+
+#         arr.sort()
+
+
+#         largest = arr[-1]
+
+#         for i in range(n-2,-1,-1):
+
+#             if arr[i] != largest:
+#                 return arr[i]
+
+
+#         return -1
+
+
+# s1 = Solution()
+
+# arr = [1,4,4,6,2,11,27,27,27,27]
+
+
+# print(s1.secondLar(arr))
+
+
 class Solution():
-    def secondLar(self,arr):
-        n = len(arr)
+    #def secondLar(self,arr):
+        # n = len(arr)
 
-        if n < 2:
-            return -1
+        # largest = arr[0]
+        # slargest = -1
 
-        arr.sort()
-
-
-        largest = arr[-1]
-
-        for i in range(n-2,-1,-1):
-
-            if arr[i] != largest:
-                return arr[i]
+        # for i in range(n):
+        #     if arr[i] > largest:
+        #         largest = arr[i]
 
 
-        return -1
+        # for i in range(n):
+        #     if arr[i] > slargest and arr[i] != largest:
+        #             slargest = arr[i]
 
 
-s1 = Solution()
+        # return slargest
 
-arr = [1,4,4,6,2,7,9,11,0,27]
+    def SecondLar(self,arr):
+        n =len(arr)
 
-print(s1.secondLar(arr))
+        largest = arr[0]
+        slargest = -1
+
+        for i in range(n):
+            if arr[i] > largest:
+                slargest = largest
+
+                largest = arr[i]
+
+
+            elif arr[i] < largest and arr[i] > slargest:
+
+                slargest = arr[i]
+
+        return slargest
+
+
+                       
+
+            
+
+
+
+
+s2 = Solution()
+
+arr = [5,9,7,8,10,10]
+
+print(s2.SecondLar(arr))
+
+
+
 
     
